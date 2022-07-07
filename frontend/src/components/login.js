ff7b48933d6d30432aa1f73b277a12f8d46c1175
MdShamim097 <shamim19119@gmail.com>
2022-07-07 20:08:07 +0600

    Modified login.js to update Log in UI

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -56,7 +56,7 @@ export default class Login extends Component {
         
               }
 
-
+/*
     render(){
         return(
             <div class="p-3 mb-2 bg-dark text-white">
@@ -96,4 +96,88 @@ export default class Login extends Component {
 
         )
     }
+  */  
+
+    render() {
+      
+      const myStyle= {
+       buttonSection:{
+        padding: "10px 40px",
+        fontSize: "20px",
+        borderRadius: "10px",
+        backgroundColor: "BlueViolet",
+        color: "white",
+       },
+
+       inputSection:{
+         padding : "10px 10px",
+       }
+
+     }
+      return (
+       
+         <div className="maincontainer">
+          <div class="container-fluid">
+              <div class="row no-gutter">
+                 
+                  
+                  <div class="col-md-12 bg-light">
+                      <div class="login d-flex align-items-center py-5">
+                         
+                          <div class="container">
+                              <div class="row align-items-center">
+                                
+                                <div class="col-lg-1">
+
+                                </div>
+
+                                <div class="col-lg-5">
+                                  <br/>
+                                  <br/>
+                                  <br/>
+                                  <br/>
+                                  <form onSubmit={this.onSubmit}>
+                                          <div class="form-group sm-3">
+                                              <input id="inputEmail" type="email" placeholder="Email address"  required="" autofocus="" value={this.state.email} onChange={this.onChangeEmail} class="form-control rounded-pill border-0 shadow-sm px-4" />
+                                          </div>
+                                          <br/>
+                                          <div class="form-group sm-3">
+                                              <input id="inputPassword" type="password" placeholder="Password" required="" value={this.state.password} onChange={this.onChangePassword} class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
+                                          </div>
+                                          <br/>
+                                          <br/>
+                                          <div class="custom-control custom-checkbox mb-3">
+                                              <input id="customCheck1" type="checkbox" class="custom-control-input"  style={myStyle.inputSection} />
+                                              <label for="customCheck1" class="custom-control-label" style={myStyle.inputSection}>Remember password</label>
+                                          </div>
+                                          <br/>
+                                          <br/>
+                                          <div className="form-group">
+                                               <input type="submit" value="Log in" className="btn btn-primary" style={myStyle.buttonSection} />
+                                          </div>
+                                
+                                   </form>
+                                </div>
+                                
+                                <div class="col-lg-1">
+
+                                </div>
+
+                                <div class="col-lg-5">
+                                      <h3 class="display-4">Welcome Back!</h3>
+                                      <br/>
+                                      <p class="text-muted mb-4">Log in to your Spacey account!</p>
+                                      <br/>
+                                  </div>
+
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+        
+  )
+  }
 }
