b5c1db185b4a445621f3a55a6ef79fb7421751bd
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2022-07-09 17:06:19 +0600

    server reload redundancy happenig in passing current user id

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-var user_id = fetch('http://localhost:5000/users/user_id').then(res => res.json()).then(data => {
-    user_id = data.user_id;
-});;
+
 
 export default class Homepage extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            user_id: 0,
+        }
+        
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:5000/users/user_id')
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    user_id: json.user_id,
+                });
+            })
+    }
 
     render() {
-        
+        const { user_id } = this.state;
         return(
             <h1>User ID: {user_id}</h1>
         )
