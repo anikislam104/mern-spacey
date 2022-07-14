866952001701c7fdd2b01d0a1c93794df0541b26
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2022-07-14 13:06:52 +0600

    error fixed

--- frontend/src/components/Blog/showBlog.js
+++ frontend/src/components/Blog/showBlog.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-var blog_id =Request.QueryString["blog_id"];
+// var blog_id =Request.QueryString["blog_id"];
 export default class ShowBlog extends Component {
     render() {
         return(
             <div>
-                <h1>Show Blog {blog_id}</h1>
+                <h1>Show Blog</h1>
             </div>
         )
     }
