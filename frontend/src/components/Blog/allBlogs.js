6ecfe5a8d3067377296fa47139487fb98cb54c66
MdShamim097 <shamim19119@gmail.com>
2022-07-17 00:34:00 +0600

    AllBlogs and MyBlogs pages are updated

--- frontend/src/components/Blog/allBlogs.js
+++ frontend/src/components/Blog/allBlogs.js
@@ -31,23 +31,62 @@ export default class AllBlogs extends Component {
     //     .then(res => console.log(res.data));
     // }
 
-     
+    GetRandomNumber (min_num, max_num){
+        return Math.floor(Math.random()* (max_num - min_num) + min_num);
+    }
+
     getAllBlogs(){
         const myStyle={
             itemSection:{
                 fontWeight: "bold",
             },
+            buttonSection1:{
+                width:"400px",
+                height:"250px",
+                fontSize: "20px",
+                textAlign: "center",
+                borderRadius: "10px",
+                backgroundColor: "#008080",
+                color: "white",
+            },
+            buttonSection2:{
+                width:"400px",
+                height:"250px",
+                fontSize: "20px",
+                textAlign: "center",
+                borderRadius: "10px",
+                backgroundColor: "Green",
+                color: "white",
+            },
+            buttonSection3:{
+                width:"400px",
+                height:"250px",
+                fontSize: "20px",
+                textAlign: "center",
+                borderRadius: "10px",
+                backgroundColor: "#004EFF",
+                color: "white",
+            },
         }
 
         var separateElements = [];
         var multiElements = this.state.blogs;
-
+        var j=0;
         for(var i = 0; i < multiElements.length; i+=3) {
            var oneRow = [];
-           oneRow.push(multiElements.slice(i, i+3).map(item => {
-           return <div class="col-lg-4" style={{display: 'inline-block'}}> <label style={myStyle.itemSection}>{item.title}</label> <br/>{item.content}</div>
-           }))
-           
+
+           if(this.GetRandomNumber(1,3)===1 && j!==1) { oneRow.push(multiElements.slice(i, i+3).map(item => {
+           return <div style={{display: "inline-block"}}>&emsp;<button style={myStyle.buttonSection1}><label style={myStyle.itemSection}>{item.title}</label> <br/>{item.content}</button></div>
+           })); j=1; }
+
+           else if(this.GetRandomNumber(1,3)===2 && j!==2) { oneRow.push(multiElements.slice(i, i+3).map(item => {
+            return <div style={{display: "inline-block"}}>&emsp;<button style={myStyle.buttonSection2}><label style={myStyle.itemSection}>{item.title}</label> <br/>{item.content}</button></div>
+           }));j=2; }
+
+            else{ oneRow.push(multiElements.slice(i, i+3).map(item => {
+                return <div style={{display: "inline-block"}}>&emsp;<button style={myStyle.buttonSection3}><label style={myStyle.itemSection}>{item.title}</label> <br/>{item.content}</button></div>
+            }));j=3;}
+  
            separateElements.push(oneRow.map(itm => {return <div><br/><div>{itm}</div><br/><br/></div>}))
         }
         return separateElements;
