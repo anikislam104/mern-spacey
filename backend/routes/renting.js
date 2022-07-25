7501d65aa75890182e8bd1a1e595962905d6393d
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2022-07-25 13:56:52 +0600

    renting route modified

--- backend/routes/renting.js
+++ backend/routes/renting.js
@@ -2,3 +2,63 @@ const router = require('express').Router();
 let User = require('../models/user');
 let Booking = require('../models/booking');
 let RentRequest = require('../models/rentRequest');
+
+router.route('/rentRequest').post(async (req, res) =>
+{
+    const host_id = req.body.host_id;
+    const renter_id = req.body.renter_id;
+    const date = req.body.date;
+    
+    const newRentRequest = new RentRequest({
+        host_id,
+        renter_id,
+        date,
+    });
+
+    await newRentRequest.save();
+}
+)
+
+router.route('/all_rentRequests').get(async (req, res) =>
+{
+    RentRequest.find()
+        .then(rentRequests => {
+            //console.log(blogs[0].content);
+            res.json(rentRequests);
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+}
+)
+
+router.route('/my_rentRequests').post(async (req, res) =>
+{
+    const renter_id = req.body.renter_id;
+    RentRequest.find({renter_id})
+        .then(rentRequests => {
+            //console.log(blogs[0].content);
+            res.json(rentRequests);
+        }).catch(err => res.status(400).json('Error: ' + err));
+}
+)
+
+router.route('/bookings').post(async (req, res) =>
+{
+    const host_id = req.body.host_id;
+    const renter_id = req.body.renter_id;
+    const date = req.body.date;
+    const property_id = req.body.property_id;
+    const time_start = req.body.time_start;
+    const time_end = req.body.time_end;
+    
+    const newBooking = new Booking({
+        host_id,
+        renter_id,
+        date,
+        property_id,
+        time_start,
+        time_end,
+    });
+
+    await newBooking.save();
+}
+)
