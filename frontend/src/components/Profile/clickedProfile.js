e8a1193507f388d31ee34e7f6329e03af9639155
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2022-08-29 10:15:24 +0600

    slight modification

--- frontend/src/components/Profile/clickedProfile.js
+++ frontend/src/components/Profile/clickedProfile.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React from "react";
 import NavbarHomepage from "../navbar_homepage";
 import { useEffect } from "react";
-import  "./viewProfile.css";
+import "./viewProfile.css";
 import { ChatState } from "../../Context/ChatProvider";
 import { useNavigate } from "react-router-dom";
 
@@ -29,7 +29,7 @@ const UserProfile = () => {
                 setComplaints(res.data.complaints);
                 setImage(res.data.image);
             })
-        
+
     }, []);
 
     //style h2 tag 
@@ -44,119 +44,120 @@ const UserProfile = () => {
     return (
         <div class="bg-light">
             <NavbarHomepage />
-            <br/>
-            <h1 class="display-6" style={{textAlign:"center"}}><b>User Profile</b></h1>
-            <br/>
+            <br />
+            <h1 class="display-6" style={{ textAlign: "center" }}><b>User Profile</b></h1>
+            <br />
 
             <div class="page-content page-container" id="page-content">
-    <div class="padding">
-        <div class="row container d-flex justify-content-center">
-<div class="col-xl-8 col-md-12">
-                      <div class="card user-card-full">
-                          <div class="row m-l-0 m-r-0">
-                              <div class="col-sm-4 bg-c-lite-green user-profile">
-                                  <div class="card-block text-center text-white">
-                                      <div class="m-b-25">
-                                      <img src={process.env.PUBLIC_URL+"/images/"+image}  
-                                      style={{  margin:"0 auto",width: "200px" , height: "200px ", display: "flex" }} alt="..."/>
-                                      </div>
-                                      <h6 class="f-w-600">{name}</h6>
-                                      
-                                      <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
-                                  </div>
-                              </div>
-                              <div class="col-sm-8">
-                                  <div class="card-block">
-                                    <br/><br/><br/>
-                                      <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
-                                      <div class="row">
-                                          <div class="col-sm-6">
-                                              <p class="m-b-10 f-w-600">Email</p>
-                                              <h6 class="text-muted f-w-400"> {email}</h6>
-                                          </div>
-                                          <div class="col-sm-6">
-                                              <p class="m-b-10 f-w-600">Phone</p>
-                                              <h6 class="text-muted f-w-400">{phone}</h6>
-                                          </div>
-                                      </div>
-                                  
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                    </div>
-                      </div>
-                  </div>
-           
-                  <div class="row align-items-center">
-          <div class="col-lg-2"></div>
-          <div class="col-lg-3">
-<p style={{textAlign:"right"}}>
-            <button
-              className="btn btn-primary"
-              style={{backgroundColor:"#ee5a6f"}}
-              onClick={() => {
-                navigate("/chat");
-              }}
-            >
-              Chat with {name}
-            </button></p>
-            </div>
+                <div class="padding">
+                    <div class="row container d-flex justify-content-center">
+                        <div class="col-xl-8 col-md-12">
+                            <div class="card user-card-full">
+                                <div class="row m-l-0 m-r-0">
+                                    <div class="col-sm-4 bg-c-lite-green user-profile">
+                                        <div class="card-block text-center text-white">
+                                            <div class="m-b-25">
+                                                <img src={process.env.PUBLIC_URL + "/images/" + image}
+                                                    style={{ margin: "0 auto", width: "200px", height: "200px ", display: "flex" }} alt="..." />
+                                            </div>
+                                            <h6 class="f-w-600">{name}</h6>
 
-            <div class="col-lg-2"></div>
+                                            <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <div class="card-block">
+                                            <br /><br /><br />
+                                            <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+                                            <div class="row">
+                                                <div class="col-sm-6">
+                                                    <p class="m-b-10 f-w-600">Email</p>
+                                                    <h6 class="text-muted f-w-400"> {email}</h6>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <p class="m-b-10 f-w-600">Phone</p>
+                                                    <h6 class="text-muted f-w-400">{phone}</h6>
+                                                </div>
+                                            </div>
 
-            <br />
-            <div class="col-lg-3">
-            <p style={{textAlign:"right"}}>
-            <button
-              className="btn btn-primary"
-              style={{backgroundColor:"#ee5a6f"}}
-              onClick={() => {
-                navigate("/showClickedBlogs");
-              }}
-            >
-              Show Properties
-            </button></p>
-            <br />
-            <p style={{textAlign:"right"}}>
-            <button
-              className="btn btn-primary"
-              
-              onClick={() => {
-                navigate("/showClickedProperties");
-              }}
-            >
-              Show Properties
-            </button></p>
-
-            </div>
-
-<br/><br/><br/>
-                  <p class="fs-4" style={{textAlign:"left"}}><b>Complaints:</b></p>
-                  <br/>
-                      <div className="card mb-5 box-shadow" style={{width:"1300px", height:"60px",backgroundColor:"white"}}>
-                  <br/>
-                    
-                        
-                       
-                        {complaints.map((complaint) => {
-                            return (
-                                <div>
-                                    <h2 style={style}>&emsp;&emsp;&emsp;&emsp;{complaint.complaint}</h2>
-                                    <br />
-                                    <br />
+                                        </div>
+                                    </div>
                                 </div>
-                            )
-                        })}
+                            </div>
+                        </div>
                     </div>
-                    
-</div>
-            
+                </div>
+            </div>
+
+            <div class="row align-items-center">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-3">
+                    <p style={{ textAlign: "right" }}>
+                        <button
+                            className="btn btn-primary"
+                            style={{ backgroundColor: "#ee5a6f" }}
+                            onClick={() => {
+                                navigate("/chat");
+                            }}
+                        >
+                            Chat with {name}
+                        </button></p>
+                </div>
+
+                <div class="col-lg-2"></div>
+
+                <br />
+                <div class="col-lg-3">
+                    <p style={{ textAlign: "right" }}>
+                        <button
+                            className="btn btn-primary"
+                            style={{ backgroundColor: "#ee5a6f" }}
+                            onClick={() => {
+                                navigate("/showClickedBlogs");
+                            }}
+                        >
+                            Show Properties
+                        </button></p>
+                    <br />
+                    <p style={{ textAlign: "right" }}>
+                        <button
+                            className="btn btn-primary"
+
+                            onClick={() => {
+                                navigate("/showClickedProperties");
+                            }}
+                        >
+                            Show Properties
+                        </button></p>
+
+                </div>
+
+                <br /><br /><br />
+                <p class="fs-4" style={{ textAlign: "left" }}><b>Complaints:</b></p>
+                <br />
+                <div className="card mb-5 box-shadow" style={{ width: "1300px", height: "60px", backgroundColor: "white" }}>
+                    <br />
+
+
+
+                    {complaints.map((complaint) => {
+                        return (
+                            <div>
+                                <h2 style={style}>&emsp;&emsp;&emsp;&emsp;{complaint.complaint}</h2>
+                                <br />
+                                <br />
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+
+        </div>
+
     )
 
-       
-   
+
+
 }
 
 export default UserProfile;
